11aa8d4fa02fdec302bbacdaaea5b54197cbe9be
nick@hirehead.com <nick@hirehead.com>
2014-04-17 23:14:27 -0500

    remove beforeAfter registration from AppServiceRegistration

--- nest/nest.d.ts
+++ nest/nest.d.ts
@@ -1,18 +1,12 @@
-declare module Nest{
+declare module Nest {
 
-	export interface AppServiceRegistration {
-		name: string;
-		key: string;
-		instance: any;
-		/**
-		* if true then by container implementation should be registered
-		* before the rest of the system
-		* if false, then after the rest of the system was registered 
-		*/
-		registerBefore?: boolean;
-	}
+    export interface AppServiceRegistration {
+        name: string;
+        key: string;
+        instance: any;
+    }
 
-	export interface INest {
-		data : Array<AppServiceRegistration>;
-	}
+    export interface INest {
+        data: Array < AppServiceRegistration > ;
+    }
 }
